2a67b848eed069afddf312b6ca4e3795f49315dc
Lotus 98T <rpitty@live.com>
2022-03-04 15:06:06 +1000

    Position state; MapContainer conditional render.

--- src/App.js
+++ src/App.js
@@ -2,28 +2,37 @@ import Search from "./Search";
 import Info from "./Info";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import patternBg from "./images/pattern-bg.png";
+import { useState } from "react";
 
 function App() {
+  const [position, setPosition] = useState([]);
+
   return (
     <div className="App">
       <header className="header-content">
         <img src={ patternBg } alt="pattern-bg" />
         <h1>IP Address Tracker</h1>
-        <Search />
+        <Search position={ position } setPosition={ setPosition } />
       </header>
       <main className="main-content">
         <Info />
-        <MapContainer id="map" center={[51.505, -0.09]} zoom={13}>
-          <TileLayer 
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+
+        {
+          position.length === 2 ?
+            (<MapContainer id="map" center={ position } zoom={13}>
+              <TileLayer 
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={ position }>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>) :
+            null
+        }
+
       </main>
     </div>
   );
